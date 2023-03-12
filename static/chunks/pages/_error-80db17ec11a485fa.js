59abd57d4c8409d02936daca4ee8d5cb364c6001
Gearonix <74734213+Gearonix@users.noreply.github.com>
2023-03-12 20:25:42 +0000

    Build: (fa3477f) started integrating socket.io on Nest.js and client.

---
+++ static/chunks/pages/_error-80db17ec11a485fa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{9715:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(59642)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9715)}),_N_E=n.O()}]);
