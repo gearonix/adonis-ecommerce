31eb06ec2396dcb964a818e220df3f0a81d7fdd4
Gearonix <74734213+Gearonix@users.noreply.github.com>
2023-03-17 19:15:07 +0000

    Build: (adfb756) application optimization. useMemo, useCallback, nextjs dynamic

---
+++ static/chunks/204.05cea1d9f6ac6595.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[204],{3204:function(e,t,s){s.r(t);var u=s(97458),f=s(52983),l=s(17418),n=s(48696);let a=(0,f.memo)(e=>{let{Component:t,...s}=e,a=(0,l.I0)(),[c,r]=(0,f.useState)([]);return(0,f.useEffect)(()=>{a((0,n.VB)(s)).then(e=>{let{payload:t}=e;t&&r(t)})},[]),(0,u.jsx)(t,{items:c,...s})});t.default=a}}]);
