9df59c339ed7335156780e3d1f0c8fd1bf760aca
Gearonix <74734213+Gearonix@users.noreply.github.com>
2023-04-01 09:19:32 +0000

    Build: (e18c943) fix: package.json

---
+++ static/chunks/2417.cc268c857ca8bbe9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2417],{62417:function(e,t,s){s.r(t);var u=s(97458),n=s(52983),c=s(64425),f=s(84672);let l=(0,n.memo)(e=>{let{Component:t,Icon:s,...l}=e,a=(0,c.I0)(),[r,h]=(0,n.useState)([]);return(0,n.useEffect)(()=>{a((0,f.VB)(l)).then(e=>{let{payload:t}=e;t&&h(t)})},[]),(0,u.jsx)(t,{items:r,Icon:s,...l})});t.default=l}}]);
---
+++ static/chunks/pages/_error-3abcbc43e655bdfd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{74402:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(85459)}])}},function(n){n.O(0,[9774,2888,179],function(){return n(n.s=74402)}),_N_E=n.O()}]);
