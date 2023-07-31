9f439dd8b38865cfa93e2f4e8781522a4c16b583
Gearonix <egor.uzhanin@gmail.com>
2023-07-31 12:12:45 +0300

    fix: fixed fatal error with google button

--- packages/client/src/widgets/Login/ui/GoogleButton.tsx
+++ packages/client/src/widgets/Login/ui/GoogleButton.tsx
@@ -1,4 +1,4 @@
-import { FC, memo } from 'react'
+import { memo } from 'react'
 import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
 import { registerByGoogle } from '../store/thunks/makeRegistration/makeRegistration'
 import { useDispatch } from 'shared/types/redux'
@@ -6,12 +6,11 @@ import { loginByGoogle } from '../store/thunks/loginUser/loginUser'
 import { GoogleButtonProps, RegisterByGoogle } from '../types'
 import { Theme } from 'shared/config/consts/themes'
 import { useTheme } from 'shared/lib/hooks/useTheme'
-import { useAdaptive } from 'shared/lib/hooks'
+import s from './style.module.scss'
 
 const GoogleButton = memo<GoogleButtonProps>(({ role }) => {
   const dispatch = useDispatch()
   const { theme } = useTheme()
-  const isMobile = useAdaptive(420)
   const onSuccess = async ({ credential }: CredentialResponse) => {
     if (role) {
       dispatch(registerByGoogle({ jwt: credential, role } as RegisterByGoogle))
@@ -21,13 +20,15 @@ const GoogleButton = memo<GoogleButtonProps>(({ role }) => {
   }
   const onError = () => console.error('Login by google failed.')
 
-  return process.env.NODE_ENV !== 'electron-build' ? <GoogleLogin
-    onSuccess={onSuccess}
-    onError={onError}
-    size={'large'}
-    width={isMobile ? '280' : '300'}
-    theme={theme === Theme.LIGHT ? undefined : 'filled_black'}
-  /> : null
+  return process.env.NODE_ENV !== 'electron-build' ? <div className={s.googleButtonWrapper}>
+    <GoogleLogin
+      onSuccess={onSuccess}
+      onError={onError}
+      size={'large'}
+      // width={isMobile ? '280' : '300'}
+      theme={theme === Theme.LIGHT ? undefined : 'filled_black'}
+    />
+  </div> : null
 })
 
 export default GoogleButton
